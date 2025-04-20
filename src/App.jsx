020951761683473b5b0ef50f9309a48c22d8544c
HASANALI117 <hassanali61911@gmail.com>
2025-04-20 03:02:55 +0300

    Update import paths for Home, Profile, WorkoutTracker, and DietTracker components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./features/home/components/Home";
+import Home from "./features/home/pages/Home";
 import Signin from "./features/auth/pages/Signin";
 import Signup from "./features/auth/pages/Signup";
 import Dashboard from "./features/dashboard/pages/Dashboard";
+import Profile from "./features/profile/Profile";
+import WorkoutTracker from "./features/dashboard/pages/WorkoutTracker";
+import DietTracker from "./features/dashboard/pages/DietTracker";
 // import Navbar from "./components/Navbar";
 
 export default function App() {
@@ -15,6 +18,9 @@ export default function App() {
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/workout" element={<WorkoutTracker />} />
+        <Route path="/diet" element={<DietTracker />} />
       </Routes>
     </Router>
   );
