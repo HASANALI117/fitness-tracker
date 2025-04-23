b50958a58cee78064d925d251be3ca5c5acdc5b7
HASANALI117 <hassanali61911@gmail.com>
2025-04-23 18:02:26 +0300

    Add age, height, weight fields and weightHistory tracking to User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,8 +4,19 @@ const UserSchema = new mongoose.Schema(
   {
     first_name: { type: String, required: true },
     last_name: { type: String, required: true },
+    age: { type: Number },
+    height: { type: Number }, // in cm
+    weight: { type: Number }, // in kg
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+
+    // Optional: Track weight changes over time
+    weightHistory: [
+      {
+        weight: Number,
+        date: { type: Date, default: Date.now },
+      },
+    ],
   },
   { timestamps: true }
 );
