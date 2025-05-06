dacf10357aa054aaf89394cb6f78adfd75cfab62
HASANALI117 <hassanali61911@gmail.com>
2025-05-06 21:10:01 +0300

    feat: restore Signin and Signup routes in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./features/home/pages/Home";
-// import Signin from "./features/auth/pages/Signin";
-// import Signup from "./features/auth/pages/Signup";
+import Signin from "./features/auth/pages/Signin";
+import Signup from "./features/auth/pages/Signup";
 import Dashboard from "./features/dashboard/pages/Dashboard";
 import Profile from "./features/profile/pages/Profile";
 import WorkoutTrackerPage from "./features/workout/pages/WorkoutTrackerPage";
 import NutritionTracker from "./features/nutrition/pages/NutritionTracker";
 import ProtectedRoutes from "./utils/ProtectedRoutes";
 import About from "./features/about/pages/About";
-import AuthPages from "./features/auth/pages/AuthPages";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/signin" element={<Signin />} /> */}
-        {/* <Route path="/signup" element={<Signup />} /> */}
-        <Route path="/signin" element={<AuthPages />} />
-        <Route path="/signup" element={<AuthPages />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
 
         <Route element={<ProtectedRoutes />}>
           <Route path="/dashboard" element={<Dashboard />} />
